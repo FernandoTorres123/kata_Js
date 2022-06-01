9fd4752257285ac69fb090515279375985451ace
FernandoTorres123 <lfernandotorres23@gmail.com>
2022-05-31 20:23:30 -0500

    primer commit de Js

---
+++ 01-intro-Js/main.js
@@ -0,0 +1,16 @@
+
+// Asi se mandan alertas desde js
+alert('Hola mien');
+
+//Asi es como se manda un mensaje  a la consola
+console.log('Hola mien');
+
+//MPstrar un error en la cnosola 
+console.error('Ya valio mien');
+
+//Mostrar un warnig en la cnosola 
+console.warn('Podria valer mien');
+
+//Pedir informacion
+prompt('Escribe tu nombre mien');
+prompt('Escribe tu apellido mien');
